4d24889af309a272b9b4bb60e4a43dd320f071b4
mpaternostro <marcelodpaternostro@gmail.com>
2019-10-08 15:38:22 -0300

    Initial commit

--- tareas/clase-4/tarea-clase-4-algoritmos.js
+++ tareas/clase-4/tarea-clase-4-algoritmos.js
@@ -10,42 +10,180 @@ Desafío de programación #1: Imprimí los números del 1 al 10
 
  Resultado: 10 9 8 7 6 5 4 3 2 1
 
- Desafìo de programación  #2: Imprimí los números impares del 1 al 100
+ */
 
- 1 3 5 ... 99
+ /*
+function contarParaArriba(numeroInicial, numeroLimite) {
+  for (let numero = numeroInicial; numero <= numeroLimite; numero++) {
+    console.log(numero);
+  }
+}
+*/
 
+// contarParaArriba(1, 10);
+
+/*
+
+Desafìo de programación  #2: Imprimí los números impares del 1 al 100
+
+1 3 5 ... 99
+*/
+
+/*
+
+let primerNumero = 1;
+let ultimoNumero = 100;
+
+function imprimirNumerosImpares(comienzo, fin) {
+  for (comienzo; comienzo <= fin; comienzo += 2) {
+    console.log(comienzo);
+  }
+}
+
+imprimirNumerosImpares(primerNumero, ultimoNumero);
+*/
+
+/*
 Desafío de programación #3: Imprimí la tabla de multiplicación del 7
 
- 7x0 = 0
- 7x1 = 7
- ...
- 7x9 = 63
+7x0 = 0
+7x1 = 7
+...
+7x9 = 63
 
-Desafío de programación #4: Imprimí todas las tablas de multiplicación del 1 al 9
 
- 1x0 = 0
- ...
- 1x9 = 9
- 2x0 = 0
- ...
- 2x9 = 18
- ...
- ...
- 9x9 = 81
+*/
 
+/* function tablaDeTalNumero(talNumero, numeroFinal) {
+  let i = 0;
+  while (i <= numeroFinal) {
+    console.log(talNumero * i);
+    i++;
+  }
+}
+let numeroTabla = 7;
+let numeroFinalTabla = 9;
+
+tablaDeTalNumero(numeroTabla, numeroFinalTabla);
+*/
+
+/*
+Desafío de programación #4: Imprimí todas las tablas de multiplicación del 1 al 9, pero que la formula
+funcione con cualquier numero al 9 (tabla de multiplicación del 1 al 15 que vaya del 1 al 9)
+
+1x0 = 0
+...
+1x9 = 9
+2x0 = 0
+...
+2x9 = 18
+...
+...
+9x9 = 81
+
+*/
+
+/*
+function tablaDeTalNumero(talNumero, numeroFinal, hastaTalNumero, matrizTabla1al9) {
+  while (talNumero <= numeroFinal) {
+    let matrizVaCambiando = [];
+    // console.log(`Imprimiendo tabla del ${talNumero}`);
+    for (let i = 0; i <= hastaTalNumero; i++) {
+      let k;
+      k = talNumero * i;
+      // console.log(k);
+      matrizVaCambiando.push(k);
+    }
+    matrizTabla1al9.push([`Tabla del ${talNumero}: ${matrizVaCambiando}`]);
+    talNumero++;
+  }
+  /* console.log(matrizTabla1al9); Lo que hizo la función es guardar las tablas de los números requeridos
+  del 1 al 9
+  // return matrizTabla1al9;
+}
+*/
+/* 
+let numeroInicialTabla = 1;
+let numeroFinalTabla = 10;
+let hastaTalNumeroTabla = 9;
+let matriz = [];
+tablaDeTalNumero(numeroInicialTabla, numeroFinalTabla, hastaTalNumeroTabla, matriz);
+console.log(matriz);
+*/
+
+/*
 Desafío de programación #5: Calcula el resultado de la suma de los numeros del 1 al 10 en un array.
- [1,2,3,4,5,6,7,8,9,10]
+[1,2,3,4,5,6,7,8,9,10]
 
- 1 + 2 + 3 + ... + 10 = ? //hacerlo con un array y un bucle
+1 + 2 + 3 + ... + 10 = ? //hacerlo con un array y un bucle
+
+*/
+
+/*
+function sumarVector(numeroInicial, numeroFinal){
+  let sumaVector = [];
+  let resultado = 0;
+  for(let i = numeroInicial; i <= numeroFinal; i++){ // Forma un vector con los numeros indicados al llamar la funcion
+    sumaVector.push(numeroInicial);
+    numeroInicial++;
+  }
+  console.log(`El vector quedó conformado asi: ${sumaVector}`);
+  for(i = 0; i < sumaVector.length; i++){ // Suma los numeros del vector en una variable "resultado"
+    resultado += sumaVector[i];
+  }
+  console.log(`El resultado de la suma de estos números es: ${resultado}`);
+}
+
+let primerNumero = 1;
+let ultimoNumero = 10;
+
+sumarVector(primerNumero, ultimoNumero);
+*/
+
+/*
 
 Desafío de programación #6: Calcula 10! (10 factorial)
 
- 10 * 9 * 8 * ... * 1
+10 * 9 * 8 * ... * 1
+
+*/
+
+/*
+function calcularFactorial(numeroFactorial){
+  let resultadoFactorial = numeroFactorial;
+  for(let i = numeroFactorial - 1; i > 0; i--){
+    resultadoFactorial *= i; 
+  }
+  console.log(`El factorial de ${numeroFactorial} es ${resultadoFactorial}`);
+}
+
+let numero = prompt("Ingrese un número");
+
+calcularFactorial(numero);
+*/
+
+/*
 
 Desafío de programación #7: Calcula la suma de todos los números impares mayores que 10 y menores que 30
 
 Desafío de programación #8: Crea una función que convierta de Celsius a Fahrenheit
 
+*/
+
+/*
+
+function calcularFarenheit(numeroCelsius){
+  let numeroFarenheit;
+  numeroFarenheit = numeroCelsius * 9 / 5 + 32
+  console.log(`Tu número (${numeroCelsius}°C) es igual a ${numeroFarenheit}°F`);
+}
+
+let numeroCelsius = prompt("Ingrese un número en grados Celsius para obtener el equivalente en grados Farenheit");
+
+calcularFarenheit(numeroCelsius);
+*/
+
+/*
 Desafío de programación #9: Crea una función que convierta de Fahrenheit a Celsius
 
 Desafío de programación #10: Calcula la suma de todos los números en un array de números
@@ -55,6 +193,24 @@ Desafío de programación #11: Calcula el promedio de todos los números en un a
 Desafío de programación #12: Crea una función que reciba un array de números y devuelva un array conteniendo solo los números positivos
 
 Desafío de programación #13: Find the maximum number in an array of numbers
+
+*/
+
+function encontrarNumeroMaximoVector(vectorDeNumeros){
+  let numeroMaximoVector = vectorDeNumeros[0];
+  console.log(numeroMaximoVector); // Este es el primer número del vector
+  for(let i = 0; i < vectorDeNumeros.length; i++){
+    if(numeroMaximoVector < vectorDeNumeros[i]){
+      numeroMaximoVector = vectorDeNumeros[i];
+    }
+  }
+  console.log(`El número máximo de este vector es: ${numeroMaximoVector}`)
+}
+
+let numerosFavoritos = [-104, 8, 10, 5, 1, 101, 32, -50, 69, -69];
+
+encontrarNumeroMaximoVector(numerosFavoritos);
+/*
 
 Desafío de programación #14: Imprimir los primeros 10 dígitos de Fibonacci sin recursión
 
@@ -66,52 +222,52 @@ Desafío de programación #15: Crear una función que encuentre el número n de 
 Desafío de programación #16: Create una función que dado un número retorne un Boolean (true/false) dependiendo si es primo o no.
 Aclaración: Los números primos son aquellos que tienen exactamente 4 divisores enteros.
 Ejemplo:
-   1 NO es primo porque tiene 2 divisores enteros (1, -1)
-   2 es primo porque sólo puede dividirse por (1,-1,2,-2)
-   3 es primo porque sólo puede dividirse por (1,-1,3,-3)
-   4 NO es primo porque se puede dividir por (1,-1,2,-2,4,-4)
-   5 es primo porque sólo puede dividirse por (1,-1,5,-5)
-   6 NO es primo porque se puede dividir por (1,-1,2,-2,3,-3,6,-6)
-   ... y así
+  1 NO es primo porque tiene 2 divisores enteros (1, -1)
+  2 es primo porque sólo puede dividirse por (1,-1,2,-2)
+  3 es primo porque sólo puede dividirse por (1,-1,3,-3)
+  4 NO es primo porque se puede dividir por (1,-1,2,-2,4,-4)
+  5 es primo porque sólo puede dividirse por (1,-1,5,-5)
+  6 NO es primo porque se puede dividir por (1,-1,2,-2,3,-3,6,-6)
+  ... y así
 
 
 Desafío de programación #17: Calcular la suma de los dígitos positivos de un número entero positivo
 Ejemplo:
-   123 = 1 + 2 + 3 = 6
-   2 = 2
-   1234 = 1 + 2 + 3 + 4 = 10
+  123 = 1 + 2 + 3 = 6
+  2 = 2
+  1234 = 1 + 2 + 3 + 4 = 10
 
 Desafío de programación #18: Imprimir los primeros 100 números primos
 
 Desafío de programación #19: Crear una función que retorne un array con los primeros números "n-primos" mayores que un número particular definido "alComienzo"
 Ejemplo:
-  Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7,11,13,17,19]
+ Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7,11,13,17,19]
 
 Desafío de programación #20: Rotar un array hacia la izquierda una posición
 Ejemplo:
-  [9,1,2,3,4] debería quedar como [1,2,3,4,9]
-  [5] debería quedar como [5]
-  [4,3,2,1] debería quedar como [3,2,1,4]
+ [9,1,2,3,4] debería quedar como [1,2,3,4,9]
+ [5] debería quedar como [5]
+ [4,3,2,1] debería quedar como [3,2,1,4]
 
 Desafío de programación #21: Rotar un array a la derecha una posición
 Ejemplo:
-  [2,3,4,1] debería quedar como [1,2,3,4]
-  [2,3] debería quedar como [3,2]
+ [2,3,4,1] debería quedar como [1,2,3,4]
+ [2,3] debería quedar como [3,2]
 
 Desafío de programación #22: Invertir un array
 Ejemplo:
-  [1,2,3,4] debería quedar como [4,3,2,1]
-  [6,5,4,3,2,1] debería quedar como [1,2,3,4,5,6]
-  [5,6,3] debería quedar como [3,6,5]
+ [1,2,3,4] debería quedar como [4,3,2,1]
+ [6,5,4,3,2,1] debería quedar como [1,2,3,4,5,6]
+ [5,6,3] debería quedar como [3,6,5]
 
 Desafío de programación #23: Invertir una cadena de caracteres
 Ejemplo:
-  "dabale arroz a la zorra el abad" debería quedar como "daba le arroz al a zorra elabad"
-  "bienvenido" debería quedar como "odinevneib"
+ "dabale arroz a la zorra el abad" debería quedar como "daba le arroz al a zorra elabad"
+ "bienvenido" debería quedar como "odinevneib"
 
 Desafío de programación #24: Crear una función que reciba dos dos arrays (arreglos) como argumentos y returne el resultado en un nuevo arreglo
 Ejemplo:
-  [1,2,3] con ["a","b","c"] debería quedar como [1,2,3,"a","b","c"]
+ [1,2,3] con ["a","b","c"] debería quedar como [1,2,3,"a","b","c"]
 
 
 Desafío de programación #25: Crear una función que reciba dos arrays (arreglos) de números como argumentos y retorne un array con números que estén en uno u otro array, pero NO en ambos.
@@ -119,17 +275,17 @@ Nota: Esto se llama "diferencia simétrica" entre conjuntos
 
 Desafío de programación #25: Crear una función que reciba dos arrays (arreglos) de números como argumentos y retorne un array con números que estén en uno u otro array, pero NO en ambos.
 Ejemplo:
-  [1,2,3] y [3,7,11] debería devolver [1,2,7,11]
-  [5,10,3] y [10,1] debería quedar como [5,3,1]
+ [1,2,3] y [3,7,11] debería devolver [1,2,7,11]
+ [5,10,3] y [10,1] debería quedar como [5,3,1]
 
 Desafío de programación #26: Crear una función que reciba dos arrays de números y retorne un nuevo array con los elementos que se encuentren en el primer array, pero no en el segundo
 Nota; Esto se llama "resta" entre conjuntos
 Ejemplo:
-  [5,1,2,3,4] y [3,4] debería devolver [5,1,2]
+ [5,1,2,3,4] y [3,4] debería devolver [5,1,2]
 
 Desafío de programación #27: Crear una función que reciba un array de números como argumento y retorne un array con los elementos distintos
 Ejemplo:
-   [1,2,3,4,5,4,3,2,1,0] debería retornar [1,2,3,4,5,0]
+  [1,2,3,4,5,4,3,2,1,0] debería retornar [1,2,3,4,5,0]
 
 
 ==== FALTAN TRADUCIR
@@ -162,4 +318,5 @@ Desafío de programación #50. Create a function that will receive n as argument
 Desafío de programación #51. Find the frequency of characters inside a string. Return the result as an array of objects. Each object has 2 fields: character and number of occurrences.
 Desafío de programación #52. Calculate Fibonacci(500) with high precision (all decimals)
 Desafío de programación #53. Calculate 70! with high precision (all decimals)
- */
+
+*/
--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -1,9 +1,32 @@
 // TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
 
+const multiplo = 3;
+const limite = 22;
+function imprimirMultiplos(numeroMultiplo, numeroLimite) {
+    let i;
+    console.log(`Imprimiendo múltiplos de ${numeroMultiplo} hasta llegar a ${numeroLimite}`);
+    for (i = numeroMultiplo; i <= numeroLimite; i += numeroMultiplo) {
+        console.log(i);
+    }
+}
+
+// imprimirMultiplos(multiplo, limite);
+
 
 // TAREA: Usando un bucle 'while', decile a tu computadora que registre los números de
 //       diez a uno.
 
+let numero;
+
+function contarParaAbajo(a) {
+    let i = 10;
+    while (i > 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+// contarParaAbajo(numero);
 
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
 // Cuenta del 1 al 50 e imprime los números:
@@ -17,5 +40,62 @@
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
 
+const numeroInicial = 1;
+const numeroFinal = 50;
+
+function fizzBuzzMuchosIf(inicio, final) {
+    const Fizz = 3;
+    const Buzz = 5;
+    const cero = 0;
+    /* está bueno trabajar con muchas variables en vez de número fijos, 
+    pero no tiene sentido poner cero = 0, no? Cero siempre va a ser cero...*/
+    for (let i = inicio; i <= final; i++) {
+        if (cero === i % Fizz && cero === i % Buzz) {
+            console.log("FizzBuzz");
+        } else if (cero === i % Fizz) {
+            console.log("Fizz");
+        } else if (cero === i % Buzz) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+function fizzBuzz(inicio, final) {
+    const Fizz = 3;
+    const Buzz = 5;
+    const cero = 0;
+    /* está bueno trabajar con muchas variables en vez de número fijos, 
+    pero no tiene sentido poner cero = 0, no? Cero siempre va a ser cero...*/
+    for (let i = inicio; i <= final; i++) {
+        let print = "";
+        if (cero === i % Fizz){
+            print += "Fizz";
+        }
+        if (cero === i % Buzz){
+            print += "Buzz";
+        }
+        console.log(print || i );
+    }
+}
+
+// fizzBuzzMuchosIf(numeroInicial, numeroFinal);
+// fizzBuzz(numeroInicial, numeroFinal);
+
 // Calcular el promedio de todos los números en un array de números. (y ponerlo en una función)
 // ej.: calcularPromedio([10,5,4,2,8])
+
+function calcularPromedio (grupoNumeros){
+    let i = 0;
+    let sumaNumeros = 0;
+    while ( i < grupoNumeros.length){
+        sumaNumeros += grupoNumeros[i];
+        i++;
+    }
+    console.log((sumaNumeros / grupoNumeros.length).toFixed(2));
+}
+
+const numerosRandom = [1, 5, 3, 2, 7, 4, 11, 5332, 4];
+
+// calcularPromedio(numerosRandom);
